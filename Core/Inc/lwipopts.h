4bfeb9e011b6b14fdc2d06842e8a76b9c5af19cd
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2022-02-05 10:20:43 -0300

    Added timeout support when sending data through sockets. TCP/IP task stack increased by 128 words.

--- Core/Inc/lwipopts.h
+++ Core/Inc/lwipopts.h
@@ -240,7 +240,7 @@ The STM32F4x7 allows computing and verifying the IP, UDP, TCP and ICMP checksums
 */
 
 #define TCPIP_THREAD_NAME               "TCP/IP"
-#define TCPIP_THREAD_STACKSIZE          1792
+#define TCPIP_THREAD_STACKSIZE          1920
 #define TCPIP_MBOX_SIZE                 6
 #define DEFAULT_UDP_RECVMBOX_SIZE       6
 #define DEFAULT_TCP_RECVMBOX_SIZE       6
@@ -252,6 +252,7 @@ The STM32F4x7 allows computing and verifying the IP, UDP, TCP and ICMP checksums
 #define SO_REUSE 1
 
 #define LWIP_SO_RCVTIMEO 1
+#define LWIP_SO_SNDTIMEO 1
 
 #define MEMP_NUM_NETCONN 16
 
